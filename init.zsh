01cc207635d94f7b0589e942781b90422c12359e
gobwas <gobwas@gmail.com>
2016-03-17 00:36:12 +0300

    tune

--- init.zsh
+++ init.zsh
@@ -1,5 +1,5 @@
-include_modules=git,go
+include_modules=go,git
 
-for file in $(/bin/zsh -c "echo ~/.dotfiles/modules/{aaa,bbb}/{${include_modules}}/{init,exports,aliases,functions,extra}.zsh"); do
+for file in $(/bin/zsh -c "echo ~/.dotfiles/modules/{${include_modules}}/{init,exports,aliases,functions,extra}.zsh"); do
 	[ -r "$file" ] && [ -f "$file" ] && source "$file";
 done;

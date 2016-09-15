c73aeb50e36480d2200f0b99d8a3e1c3e60d2857
s.kamardin <s.kamardin@corp.mail.ru>
2016-09-15 21:07:55 +0300

    fix gopath

--- modules/go/exports.zsh
+++ modules/go/exports.zsh
@@ -1,5 +1,5 @@
 # unalias gb cause it overlaps gb binary
 [[ "$(type -a gb | grep "alias")" != "" ]] && unalias gb
 
-export GOPATH=~/Development/go
+export GOPATH=~/go
 export PATH=$GOPATH/bin:$PATH
